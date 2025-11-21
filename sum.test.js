cc80e1e8df3afc077464f3a764602eab79438e62
LeeVuKhang <lvukhangpq@gmail.com>
2025-11-21 13:40:19 +0700

    test action

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,7 @@
 const sum = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('adds 1 + 3 to equal 4', () => {
+  expect(sum(1, 3)).toBe(4);
 });
 
 test('adds 0 + 0 to equal 0', () => {
